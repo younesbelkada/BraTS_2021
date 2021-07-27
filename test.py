077625897d556dace3f6063969efe299f891ad72
arthurzucker <arthur.zucker@gmail.com>
2021-07-27 21:23:36 +0200

    loader and gitignore

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 from utils.dataset import *
 # create a config file 
-path_dataset = ''
-path_csv = ''
-tool_name = 'FLAIR'
-test_dataset = BraTS_Dataset('train', path_dataset, path_csvm, tool_name)
+path_dataset = './data'
+path_csv = './data/train_labels.csv'
+tool_name = 'T2w'
 
+test_dataset = BraTS_Dataset('train', path_dataset, path_csv, tool_name)
+mi,ma=1000,-1
 for img, label in test_dataset:
-    print(img.shape)
-    print(label)
-    exit(0)
+    mi = min(img.shape[0],mi)
+    ma = max(img.shape[0],ma)
+    print(np.min(img),np.max(img))
+print(mi,ma)
--- utils/dataset.py
+++ utils/dataset.py
@@ -16,15 +16,16 @@ def read_image(path_img, voi_lut = True, fix_monochrome = True):
     if voi_lut:
         data = apply_voi_lut(dicom.pixel_array, dicom)
     else:
-        data = dicom.pixel_array
-               
+         data = dicom.pixel_array
+    data = dicom.pixel_array        
     # depending on this value, X-ray may look inverted - fix that:
     if fix_monochrome and dicom.PhotometricInterpretation == "MONOCHROME1":
         data = np.amax(data) - data
-        
+
     data = data - np.min(data)
-    data = data / np.max(data)
-    #data = (data * 255).astype(np.uint8)
+    if np.max(data) != 0:
+        data = data / np.max(data)
+    data = (data * 255).astype(np.uint8)    #data = (data * 255).astype(np.uint8)
         
     return data
 
@@ -34,22 +35,23 @@ class BraTS_Dataset(Dataset):
         self.path = path
         self.tool = tool_name
         self.path_csv = path_csv
-        self.ext = '.dcm'
+        self.ext = '*.dcm'
         self.preprocess()
+        self.transform = None
+
     def preprocess(self):
         self.path_to_images = []
         self.image_labels = []
-        self.csv_df = pd.read_csv(self.path_csv)
-
+        self.csv_df = pd.read_csv(self.path_csv,header=0,index_col="BraTS21ID")
 
         array_data = os.listdir(os.path.join(self.path, self.split))
         
         for subjects in array_data:
-            if self.tool_name:
+            if self.tool:
                 path_to_images = glob(os.path.join(self.path, self.split, subjects, self.tool, self.ext))
                 self.path_to_images.extend(path_to_images)
-                #label = self.csv_df[self.csv_df['BraTS21ID'] == subjects]
-                label = self.csv_df['BraTS21ID'][subjects].MGTM_value
+                # label = self.csv_df.loc[self.csv_df['BraTS21ID'] == subjects]
+                label = self.csv_df.loc[int(subjects)].MGMT_value
                 tab_labels = [label for i in range(len(path_to_images))]
                 self.image_labels.extend(tab_labels)
         
