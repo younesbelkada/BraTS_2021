13f5ef3cf568924cce9887cb170cb885a79bcb26
ArthurZucker <48595927+ArthurZucker@users.noreply.github.com>
2021-08-04 15:32:14 +0200

    Delete test.py

--- younes_previous_code/test.py
+++
@@ -1,7 +0,0 @@
-from utils.dataset import *
-# create a config file 
-path_dataset = './data'
-path_csv = './data/train_labels.csv'
-tool_name = 'T2w'
-
-test_dataset = BraTS_Dataset('train', path_dataset, path_csv, tool_name)
